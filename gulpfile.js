3d5b69e785ec4b88fa6e9bdaca95126275c541c8
Anthony Graule <anthony.graule@gmail.com>
2024-04-09 23:10:59 +0200

    Update to gulp 4 and gulp sass 5 + javascript ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,53 @@
-/*
-* Use Gulp to compile sass files 
-* @date: 2016
-* @author: Anthony graule
-*
-* Need a folder 'assets' containing two folders named 'css' and 'sass'
-* gulpfile.js must to be at the same assets level with package.json
-* Open nodejs command and use 'npm install' to install node_modules
-* nodejs and gulp must to be install before
-*/
+/**
+ * @description Use Gulp to compile and to minify sass files
+ * @date: 2024
+ * @author: Anthony graule <anthony.graule@gmail.com>
+ *
+ * Need a folder 'assets' containing two folders named 'css' and 'sass'
+ * gulpfile.js must to be at the same assets level with package.json
+ * Open nodejs command and use 'npm install' to install node_modules
+ * nodejs 14, python 3 and gulp must to be install before
+ */
 
-// Requires gulp
-var gulp = require('gulp');
-// Include plugins
-var plugins = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass')(require('sass'));
+// Require plugins
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
-//To compile sass in css(dev)
-gulp.task('sass', function(){
-  return gulp.src('assets/sass/**/*.+(scss|sass)')
-    .pipe(plugins.sass())
-	.pipe(plugins.csscomb())
-	.pipe(plugins.cssbeautify({indent: '  '}))
-	.pipe(plugins.autoprefixer())
-    .pipe(gulp.dest('assets/css'))
-});
+/**
+ * @description Permet de compiler tous les fichiers SCSS en CSS
+ * @returns {PassThrough}
+ */
+function sass() {
+    return gulp.src('sass/style.+(scss|sass)').pipe(gulpSass().on('error', gulpSass.logError)).pipe(prefix()).pipe(minify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('css')).pipe(browserSync.stream())
+}
 
-//To watch sass (dev)
-gulp.task('watch', function(){
-  gulp.watch('assets/sass/**/*.+(scss|sass)', ['sass']); 
-  // Other watchers
-})
+/**
+ * @description Détecte les changements et compile en live en faisant appel à la méthode sass()
+ */
+function watch() {
+    gulp.watch('sass/**/*.+(scss|sass)', sass);
+}
 
-// To minify (production)
-gulp.task('minify', function () {
-  return gulp.src('assets/css/*.css')
-    .pipe(plugins.csso())
-    .pipe(plugins.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('assets/css'));
-});
+/**
+ * @description Détecte les changements et compile en live en faisant appel à la méthode sass() et recharge automatiquement
+ * la page du navigateur. Il exécute aussi le reload JS
+ */
+function reload() {
+    browserSync.init({
+        proxy: {target: 'https://www.lesjardinsdetouraine.fr'}
+    });
+    gulp.watch('sass/**/*.+(scss|sass)').on('change', browserSync.reload);
+    gulp.watch('../js/**/*.js').on('change', browserSync.reload);
+}
+
+/**
+ * Définition des différentes tasks à appeler pour exécuter les commandes
+ * @type {function(): PassThrough}
+ */
+exports.sass = sass;
+exports.watch = gulp.series(sass, watch);
+exports.reload = gulp.series(sass, reload);
--- readme.md
+++ readme.md
@@ -1,14 +1,11 @@
 # Gulp-SASS
 
 Use Gulp to compile sass files <br>
-@date: 2016<br>
+@date: 2024<br>
 @author: Anthony graule
 
-* nodejs and gulp must to be install before
-* to install gulp : npm install gulp -g
-* Need a folder 'assets' containing two folders named 'css' and 'sass'
+* nodejs 14, python 3 and gulp must to be install before
+* need a folder 'assets' containing two folders named 'css' and 'sass'
 * gulpfile.js must to be at the same assets level with package.json
-* Open nodejs command and use 'npm install' to install node_modules
+* use 'npm install' to install node_modules
 
-
-![Gulp-SASS](https://camo.githubusercontent.com/378c32003fef4a4028debed1534bd4ae3315602b/68747470733a2f2f7777772e6c69717569646c696768742e636f2e756b2f66696c6561646d696e2f5f70726f6365737365645f2f63736d5f7376672d7370726974652d76325f366138316664353337662e706e67 "Gulp-SASS")
